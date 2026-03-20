df60517a8c4b6fcaf6e23aca95ffd667f3e0ae72
Maca <maca.esc.k@gmail.com>
2026-03-19 21:14:58 -0300

    Modifica saludo para incluir edad

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-function saludo(nombre, mensaje) {
-console.log(`${mensaje}, ${nombre}!`);
+function saludo(nombre, edad) {
+console.log(`Hola ${nombre}, tienes ${edad} años.`);
 }
-
-saludo("Rosario", "¡Hola");
-saludo("Pepe", "¡Buen trabajo");
